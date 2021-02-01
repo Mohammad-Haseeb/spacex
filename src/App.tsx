f533eb3e738faea54f21ebd7e6b59ab9d501428a
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-02 01:33:39 +0500

    logo add

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import MissionList from './components/MissionList/index';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { CompoleteDetails} from './components/FinalMissionDetails/index'
+import logo from './logo.png';
 
 function App() {
   return (
-   
+        <>
+            <img src={logo}  className="logo" alt="logo"/>
+            <br/>
      <Routes>
       <Route path="/" element={<MissionList />} />
       <Route path="missionDetails/:invoiceId" element={<CompoleteDetails/>} />
      
     </Routes>
-     
+     </>
     
   );
 }
